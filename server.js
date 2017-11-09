0a225ee6b2afe7e2eeb414b46ae0fea53de97141
Sigurd skole-pc <sschoeler99@gmail.com>
2017-11-09 18:44:22 +0100

    added controller, model, routes, and server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express"),
+	app = express(),
+	port = process.env.PORT || 3000,
+	mongoose = require("mongoose"),
+	Task = require("./api/models/todoListModel"),
+	bodyParser = require("body-parser");
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/Tododb");
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/*app.use(function(req, res) {
+	res.status(404).send({ url: req.originalUrl + " not found!" });
+});*/
+
+var routes = require("./api/routes/todoListRoutes");
+routes(app);
+
+app.listen(port);
+
+console.log("todo app REST api started on port: " + port);
